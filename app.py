0c19e6fd083010df5aeb7afac66db6cddefb84e4
Питерских Сергей Алеексевич <piterskihsa@altarix.ru>
2018-04-26 00:47:41 +0300

    Update file

--- app.py
+++ app.py
@@ -1,114 +1,15 @@
 import os
 from datetime import datetime
 from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from flask.ext.wtf import Form
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.bootstrap import Bootstrap
+# from flask.ext.moment import Moment
+# from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.mail import Mail, Message
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-from flask import Flask, render_template, session, redirect, url_for, flash
 
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)
-
-# app.config['SECRET_KEY'] = 'hard to guess string'
-# app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-# app.config['MAIL_PORT'] = 587
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-# app.config['SOCIAL_MAIL_SUBJECT_PREFIX'] = '[Social]'
-# app.config['SOCIAL_MAIL_SENDER'] = 'Social Admin <social@example.com>'
-# app.config['SOCIAL_ADMIN'] = os.environ.get('SOCIAL_ADMIN')
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-db = SQLAlchemy(app)
-mail = Mail(app)
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
-
-
-def send_mail(to, subject, template, **kwargs):
-    mail_message = Message(app.config['SOCIAL_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['SOCIAL_MAIL_SENDER'],
-                           recipients=[to])
-    mail_message.body = render_template(template + '.txt', **kwargs)
-    mail_message.html = render_template(template + '.html', **kwargs)
-    mail.send(mail_message)
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-class NameForm(Form):
-    name = StringField("Ваше Имя?", validators=[Required()])
-    submit = SubmitField("Submit")
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username = form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if app.config['SOCIAL_ADMIN']:
-                send_mail(app.config['SOCIAL_ADMIN'], 'New User', 'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-# @app.route('/user/<id:int>')
-# def get_user(id):
-#     user = load_user(id)
-#     if not user:
-#         abort(404)
-#     return '<h1>Hello, %s!</h1>' % user.name
-
-if __name__ == '__main__':
-    manager.run()
+# def send_mail(to, subject, template, **kwargs):
+#     mail_message = Message(app.config['SOCIAL_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['SOCIAL_MAIL_SENDER'],
+#                            recipients=[to])
+#     mail_message.body = render_template(template + '.txt', **kwargs)
+#     mail_message.html = render_template(template + '.html', **kwargs)
+#     mail.send(mail_message)
--- config.py
+++ config.py
@@ -14,14 +14,14 @@ class Config():
     SOCIAL_ADMIN = os.environ.get('SOCIAL_ADMIN')
 
     @staticmethod
-    def __init__(self):
+    def init_app(app):
         pass
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -33,11 +33,11 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
 
-    SQLALCHEMY_DATABASE_URL = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
