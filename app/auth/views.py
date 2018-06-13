f7535c03f53363fce9436c021e09e8fa3172a5f5
Piterskikh Sergey <piterskikhsa@mail.ru>
2018-06-13 12:23:25 +0300

    update all project

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,9 @@ from flask.ext.login import login_user, login_required, logout_user
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from ..models import User
+from ..email import send_email
+from .. import db
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,9 +29,17 @@ def logout():
     return redirect(url_for('main.index'))
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User()
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Подтвердите ваш email', 'auth/email/confirm', user=user, token=token)
+        flash('Письмо с подтверждением отправлено вам по электронной почте')
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
